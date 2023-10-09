b207ae3dcf63ec2413621e31852d5db62737077b
Selina <ngatuyet096@gmail.com>
2023-10-10 00:26:43 +0700

    Fist commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="app-container">
+    <div id="lightgallery">
+      <a
+        data-lg-size="1600-1067"
+        data-src="https://phongvu.vn/cong-nghe/wp-content/uploads/sites/2/2018/07/hinh-nen-full-hd-cho-laptop-1.jpg"
+      >
+        <img
+          style="width: 300px; height: 300px; object-fit: cover;"
+          src="https://phongvu.vn/cong-nghe/wp-content/uploads/sites/2/2018/07/hinh-nen-full-hd-cho-laptop-1.jpg"
+        />
+      </a>
+
+      <a
+        data-lg-size="1600-1067"
+        data-src="https://img.freepik.com/premium-photo/closeup-nature-view-colorful-leaves_860528-2622.jpg"
+      >
+        <img
+          style="width: 300px; height: 300px; object-fit: cover;"
+          src="https://img.freepik.com/premium-photo/closeup-nature-view-colorful-leaves_860528-2622.jpg"
+        />
+      </a>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import lightGallery from "lightgallery";
+// Plugins
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+
+  mounted() {
+    const el = document.getElementById('lightgallery')
+    window.lightGallery(el, {
+        mode: 'lg-slide',
+        speed: 500,
+        autoplayFirstVideo: false,
+        pager: false,
+        galleryId: "nature",
+        autoplayFirstVideo: true,
+        plugins: [lgZoom, lgThumbnail],
+        enableSwipe: true, // required
+        showMaximizeIcon: true, // required
+        mobileSettings: {
+          controls: false,
+          showCloseIcon: false,
+          download: false,
+          rotate: false
+        }
+      })
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import lightgallery from 'lightgallery.js'
+import 'lightgallery.js/dist/css/lightgallery.min.css'
+
+Vue.use(lightgallery)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
